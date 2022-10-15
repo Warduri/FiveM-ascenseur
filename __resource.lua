c9f3cb835be1a2496fff7020bfd74096656395ad
Warduri <115869227+Warduri@users.noreply.github.com>
2022-10-15 11:42:21 +0200

    Ressource manifest
    Description du script

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,8 @@
-resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af93'
+
+author 'Jules Vance'
+description 'Script ascenseur'
+version '1.0'
 
 client_scripts {
     "@NativeUI/NativeUI.lua",
